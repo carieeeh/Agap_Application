06052612ed409d5223256ff7fb6fd03ec40edbac
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-01-13 02:23:22 +0800

    feat: profile page ui

--- lib/layout/private/profile/profile.dart
+++ lib/layout/private/profile/profile.dart
@@ -1,4 +1,7 @@
+import 'package:agap_mobile_v01/global/constant.dart';
+import 'package:agap_mobile_v01/layout/private/main_container.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -10,6 +13,100 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MainContainer(
+      title: "Profile",
+      body: SizedBox(
+        height: Get.height,
+        child: Stack(
+          children: [
+            Container(
+              height: Get.height * .2,
+              decoration: const BoxDecoration(
+                color: primaryRed,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(100)),
+              ),
+            ),
+            Positioned(
+              top: 80,
+              left: 50,
+              child: Container(
+                height: Get.height * .15,
+                width: Get.width * .7,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: gray),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                    child: Text(
+                  "User Name",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                )),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: Get.width * .38,
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.white,
+                child: Image.asset(
+                  'assets/images/person.png',
+                  fit: BoxFit.cover,
+                  height: 45,
+                ),
+              ),
+            ),
+            Positioned(
+              top: Get.height * .3,
+              width: Get.width,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text(
+                      "About Me",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    subtitle: const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 15, 0, 0),
+                      child: Text(
+                          "Lorem ipsum dolor sit amet. Aut velit dolorem et magni suscipit est accusamus"),
+                    ),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    title: const Text(
+                      "Allergies",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    subtitle: const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 15, 0, 0),
+                      child: Text(
+                          "Lorem ipsum dolor sit amet. Aut velit dolorem et magni suscipit est accusamus"),
+                    ),
+                    onTap: () {},
+                  ),
+                  const Divider(),
+                  ListTile(
+                    title: const Text(
+                      "Blood type",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    subtitle: const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 15, 0, 0),
+                      child: Text(
+                          "Lorem ipsum dolor sit amet. Aut velit dolorem et magni suscipit est accusamus"),
+                    ),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

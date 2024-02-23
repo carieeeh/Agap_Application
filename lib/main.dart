1cc7af4d2e899a1e51a7e4c58900b2abc7b01f9a
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-02-24 00:21:06 +0800

    firebase connection with mobile app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:agap_mobile_v01/router/route_list.dart';
 import 'package:agap_mobile_v01/services/auth_service.dart';
 import 'package:agap_mobile_v01/services/setting_service.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Get.putAsync(() => AuthService().init());
   await Get.putAsync(() => SettingService().init());
